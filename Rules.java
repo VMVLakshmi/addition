fd4a1aaa3f549f4528eb85af6b78b38c3d22d569
VMVLakshmi <88193171+VMVLakshmi@users.noreply.github.com>
2021-08-01 20:24:08 +0530

    Add files via upload

---
+++ Rules.java
@@ -0,0 +1,13 @@
+package datatypes;
+
+public class Rules {
+
+	public static void main(String[] args) {
+		byte b=4;
+		float f=5.5f;
+		float result=(f*b);
+		System.out.println(result);
+
+	}
+
+}
