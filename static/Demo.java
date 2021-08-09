df74798120d8bb45bb1088548ccaafb7b01659bc
VMVLakshmi <88193171+VMVLakshmi@users.noreply.github.com>
2021-08-09 19:44:21 +0530

    Add files via upload

---
+++ static/Demo.java
@@ -0,0 +1,12 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		  SimpleCalculator obj = new SimpleCalculator();
+	       System.out.println(obj.add(10, 20));
+	       System.out.println(obj.add(10, 20, 30));
+	   
+
+	}
+
+}
---
+++ static/SimpleCalculator.java
@@ -0,0 +1,14 @@
+
+public class SimpleCalculator {
+	
+	    int add(int a, int b)
+	    {
+	         return a+b;
+	    }
+	    int  add(int a, int b, int c)  
+	    {
+	         return a+b+c;
+	    
+	    }
+
+}
