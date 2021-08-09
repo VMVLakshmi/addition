d933afdcb06326502b1c7a02c1ccf44c1a1a6b46
VMVLakshmi <88193171+VMVLakshmi@users.noreply.github.com>
2021-08-09 19:45:20 +0530

    Add files via upload

--- Human.java
+++ Human.java
@@ -1,22 +1,22 @@
 
 public class Human {
-	public static void walk()
+	public void walk()
 	   {
 	       System.out.println("Human walks");
 	   }
 	}
-	class Boy extends Human{
-	   public static void walk(){
+	class Demo extends Human{
+	   //Overriding Method
+	   public void walk(){
 	       System.out.println("Boy walks");
 	   }
 	   public static void main( String args[]) {
 	    
-	       Human obj = new Boy();
-	       
+	       Human obj = new Demo();
+	  
 	       Human obj2 = new Human();
 	       obj.walk();
 	       obj2.walk();
-	   }
-	}
 
-
+}
+}
